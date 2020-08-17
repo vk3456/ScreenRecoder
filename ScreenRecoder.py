869bc623e739295a43e8193dc3efcff4b8bc23ed
vk3456 <65508543+vk3456@users.noreply.github.com>
2020-08-17 18:48:20 +0930

    Add files via upload
    this is the screenRecoder project but it's use to coordinate and easily take screenRecoder provide in so that i will ready this project

---
+++ ScreenRecoder.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+from PIL import ImageGrab
+
+
+def screenRecoder():
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter("output.avi",fourcc,5.0,(1366,786))
+
+
+    while True:
+        img = ImageGrab.grab()
+
+        img_np = np.array(img)
+        frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+        out.write(frame)
+
+        if cv2.waitKey(1)==27:
+            break
+    out.release()
+    cv2.destroyAIIWindows()
+screenRecoder()        
